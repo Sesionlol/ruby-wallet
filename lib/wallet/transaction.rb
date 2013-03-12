eea48ca8e65f5d4213cb7e6d89d05f72bc949b97
oGMinor <ogminor@gmail.com>
2013-03-12 05:57:57 -0700

    Added address to transaction.

--- lib/wallet/transaction.rb
+++ lib/wallet/transaction.rb
@@ -15,18 +15,12 @@ module RubyWallet
       @wallet = wallet
       @account = wallet.accounts.new(args[:account])
       @id = args[:txid]
-      @address_str = args[:address]
+      @address = args[:address]
       @amount = args[:amount]
       @confirmations = args[:confirmations]
       @occurred_at = Time.at(args[:time])
       @received_at = Time.at(args[:timereceived])
       @category = args[:category]
-    end
-
-    def address
-      @address ||= @account.addresses.find do |address|
-        address.address == @address_str
-      end
     end
 
   end
