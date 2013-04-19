0c6fa6f41efba4c1cb6cf3c44068da6f514dfd91
oGMinor <ogminor@gmail.com>
2013-04-19 11:46:21 -0700

    Updated versioning

--- lib/bitcoin/version.rb
+++ lib/bitcoin/version.rb
@@ -2,7 +2,7 @@ module Bitcoin
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 7
+    PATCH = 8
     REL = nil
     STRING = REL ? [MAJOR, MINOR, PATCH, REL].join('.') : [MAJOR, MINOR, PATCH].join('.')
   end
